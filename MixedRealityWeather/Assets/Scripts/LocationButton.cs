a01878419325fd5046ce1e9bfa7781868fc53ef1
h-rost <hannah.rost@rwth-aachen.de>
2020-05-02 16:57:41 +0200

    added LocationButton script

---
+++ MixedRealityWeather/Assets/Scripts/LocationButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities.Editor;
+using Microsoft.MixedReality.Toolkit.Utilities.Editor.Search;
+using Microsoft.MixedReality.Toolkit.Input;
+using TMPro;
+
+public class LocationButton: IMixedRealityPointerHandler
+{
+    public TextMeshPro text;
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log(text);
+    }
+
+    void IMixedRealityPointerHandler.OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
