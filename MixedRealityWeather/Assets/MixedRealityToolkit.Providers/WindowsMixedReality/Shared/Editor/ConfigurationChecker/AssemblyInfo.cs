d0c30e976ca05429c57bd9132dfc88c8f240983e
h-rost <hannah.rost@rwth-aachen.de>
2020-05-05 14:56:38 +0200

    fixed buttons

--- MixedRealityWeather/Assets/MixedRealityToolkit.Providers/WindowsMixedReality/Shared/Editor/ConfigurationChecker/AssemblyInfo.cs
+++ MixedRealityWeather/Assets/MixedRealityToolkit.Providers/WindowsMixedReality/Shared/Editor/ConfigurationChecker/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+//[assembly: AssemblyFileVersion("2.3.0.0")]
 
 [assembly: AssemblyProduct("Microsoft® Mixed Reality Toolkit Providers")]
 [assembly: AssemblyCopyright("Copyright © Microsoft Corporation")]
