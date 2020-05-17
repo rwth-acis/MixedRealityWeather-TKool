4a38abfb1573cf41e2c813325a3c116ecc5acd35
h-rost <hannah.rost@rwth-aachen.de>
2020-05-17 16:21:54 +0200

    bis inklusive d)

---
+++ MixedRealityWeather/Assets/Scripts/BoundingBoxState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities.Editor;
+using Microsoft.MixedReality.Toolkit.Utilities.Editor.Search;
+
+public class BoundingBoxState : MonoBehaviour
+{
+    public Microsoft.MixedReality.Toolkit.UI.BoundingBox box;
+    bool boxState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      boxState= box.Active;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (boxState)
+        {
+            box.GetComponent<BoxCollider>().enabled = false;
+        }
+        else
+        {
+            box.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
